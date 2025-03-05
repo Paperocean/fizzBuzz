fa3d61c2e2cfbcf83d1696e47389331c952b5249
Michał Wróblewski <272488@student.pwr.edu.pl>
2025-03-05 17:44:13 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿class Program
+{
+    static void Main()
+    {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        fizzBuzz.Run(10);
+
+        WelcomeMessage message = new WelcomeMessage();
+        message.Hello();
+    }
+}
+
+class WelcomeMessage
+{
+    public void Hello()
+    {
+        Console.WriteLine("Hello World");
+    }
+}
+
+class FizzBuzz
+{
+    public void Run(int n)
+    {
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (i % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
